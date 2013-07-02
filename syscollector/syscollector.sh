b71cac855dd8a736236f4bb13143ee2340a3170d
gflores <gus.gh@gmfcomp.com>
2013-07-02 17:42:26 -0400

    Added parse of CPU

--- syscollector/syscollector.sh
+++ syscollector/syscollector.sh
@@ -2,7 +2,11 @@
 
 
 function cpu_data {
-	ssh $1 'uname -a'
+	kernel=`ssh $1 'uname -sr'`
+	CPU=`ssh $1 'cat /proc/cpuinfo | grep processor| tail -1'`
+	model=`ssh $1 'cat /proc/cpuinfo | grep "model name"| tail -1'`
+	freq=`ssh $1 'cat /proc/cpuinfo | grep "cpu MHz"| tail -1'`
+	echo -e "$kernel\n $CPU\n $model\n $freq\n"
 	exit
 }
 
