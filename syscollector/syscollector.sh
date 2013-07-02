9d557f5218b2194d61be9be04ad50e35ba2ffb0a
gflores <gus.gh@gmfcomp.com>
2013-07-02 18:36:32 -0400

    Parse for memory added. Also added swap of spaces/newline logic.

--- syscollector/syscollector.sh
+++ syscollector/syscollector.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
 
+# For the functons we swap the spaces and new lines for a placeholder.
+
 function cpu_data {
-	kernel=`ssh $1 'uname -sr'`
+	kernel=`ssh $1 'uname -sr| sed "s/[[:blank:]]\+/#/g"'`
 	CPU=`ssh $1 'cat /proc/cpuinfo | grep processor| tail -1'`
 	model=`ssh $1 'cat /proc/cpuinfo | grep "model name"| tail -1'`
 	freq=`ssh $1 'cat /proc/cpuinfo | grep "cpu MHz"| tail -1'`
-	echo -e "$kernel\n $CPU\n $model\n $freq\n"
-	exit
+	cpudata=`echo ${kernel}!${CPU}!${model}!${freq}|sed 's/[[:blank:]]\+/#/g'`
+	echo $cpudata
 }
 
-cpu_data $1
+
+function mem_data {
+	memory=`ssh $1 'cat /proc/meminfo | grep MemTotal| sed "s/[[:blank:]]\+/#/g"'`
+	memdata=`echo ${memory}| sed 's/[[:blank:]]\+/#/g'`	
+	echo "$memdata"
+}
+
+cpu_data $1 | sed 's/\#/ /g' | sed 's/\!/\n/g'
+mem_data $1 | sed 's/\#/ /g' | sed 's/\!/\n/g'
+
